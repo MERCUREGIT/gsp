e60736c3e7bfe3133be293d9b82a4c7cd6ac395e
mercuregit <ngumbukafon@gmail.com>
2021-04-22 17:48:54 +0100

    update of services

--- src/components/sections/servicetwo/Features.js
+++ src/components/sections/servicetwo/Features.js
@@ -1,48 +1,53 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import img1 from '../../../assets/img/feature/01.png'
-import img2 from '../../../assets/img/feature/02.jpg'
-import img3 from '../../../assets/img/feature/03.jpg'
-import img4 from '../../../assets/img/feature/04.jpg'
-import img5 from '../../../assets/img/feature/05.jpg'
-import img6 from '../../../assets/img/feature/06.jpg'
+import line1 from '../../../assets/img/lines/12.png'
+import line2 from '../../../assets/img/lines/11.png'
 
-const featuresblock = [
-    { img: img1, title: 'printing', text: 'Join us for printing' },
-    { img: img2, title: 'Business Growth', text: 'Join us for consultatoins' },
-    { img: img3, title: 'IT Consulting', text: 'Join us for consultatoins' },
-    { img: img4, title: 'Investment Plan', text: 'Join us for consultatoins' },
-    { img: img5, title: 'Financial Planning', text: 'Join us for consultatoins' },
-    { img: img6, title: 'Relationship', text: 'Join us for consultatoins' },
+const serviceblock = [
+    { icon: 'flaticon-teamwork-1', title: 'Education' , link :"services/education" },
+    { icon: 'flaticon-chart', title: 'Printing', link: 'services/printing' },
+    // { icon: 'flaticon-graph-1', title: 'Marketing Strategy' },
+    // { icon: 'flaticon-money', title: 'Financial Plan' },
+    { icon: 'flaticon-diamond', title: 'Raw material', link: 'services/material' },
+    // { icon: 'flaticon-teamwork-1', title: 'NGO', ink: '/ngo' },
+    { icon: 'flaticon-diagram', title: 'Renewable Energy', link: 'services/energy' },
+    { icon: 'flaticon-setup', title: 'Building', link: 'services/building'  },
+    { icon: 'flaticon-pie-chart', title: 'Website Consulting', link: 'services/Websites'},
 ]
 
 
 export default () => {
-
     return (
-        <section className="feature-section section-gap grey-bg">
+        <section className="service-section shape-style-two service-line-shape section-gap grey-bg">
             <div className="container">
+                {/* Section Title */}
                 <div className="section-title text-center both-border mb-50">
-                    <span className="title-tag"> Popular Service</span>
-                    <h2 className="title">Our Awesome Services To <br /> Grow Your Business</h2>
+                    <span className="title-tag">Comany Services</span>
+                    <h2 className="title">We Provide Most Exclusive <br /> Service For Business</h2>
                 </div>
-                {/* Feature boxes */}
-                <div className="feature-boxes row justify-content-center">
-                    {featuresblock.map((item, i) => (
-                        <div key={i} className="col-lg-4 col-md-6 col-10 col-tiny-12">
-                            <div className="feature-box">
-                                <div className="feature-bg bg-img-c" style={{ backgroundImage: "url(" + item.img + ")" }}>
+                {/* Services Boxes */}
+                <div className="row service-boxes justify-content-center">
+                    {serviceblock.map((item, i) => (
+                        <div key={i} className="col-lg-4 col-sm-6 col-10 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="1000ms">
+                            <div className="service-box-two text-center">
+                                <div className="icon">
+                                    <i className={item.icon} />
                                 </div>
-                                <div className="feature-desc">
-                                    <Link to="/servicedetail" className="feature-link"><i className="fal fa-long-arrow-right" /></Link>
-                                    <h4>{item.title}</h4>
-                                    <p>{item.text}</p>
-                                </div>
+                                <h3><Link to={"/" + item.link}> {item.title}</Link></h3>
+                                <Link to={"/" + item.link} className="service-link">
+                                    <i className="fal fa-arrow-right" />
+                                </Link>
                             </div>
                         </div>
                     ))}
                 </div>
+            </div>
+            <div className="line-one">
+                <img src={line1} alt="line-shape" />
+            </div>
+            <div className="line-two">
+                <img src={line2} alt="line-shape" />
             </div>
         </section>
     )
--- src/components/sections/servicetwo/Service.js
+++ src/components/sections/servicetwo/Service.js
@@ -1,45 +1,53 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import icon1 from '../../../assets/img/icons/01.png'
-import icon2 from '../../../assets/img/icons/02.png'
-import icon3 from '../../../assets/img/icons/03.png'
-import icon4 from '../../../assets/img/icons/04.png'
+import line1 from '../../../assets/img/lines/12.png'
+import line2 from '../../../assets/img/lines/11.png'
 
 const serviceblock = [
-    { icon: icon1, title: 'Creative Idea', text: 'Sed perspicia unde omnis' },
-    { icon: icon2, title: 'Business Strategy', text: 'Quis autem velrepres hend' },
-    { icon: icon3, title: 'Relationship', text: 'Sed perspicia unde omnis' },
-    { icon: icon4, title: 'Productivity', text: 'Quis autem velrepres hend' },
+    { icon: 'flaticon-teamwork-1', title: 'Education' , link :"services/education" },
+    { icon: 'flaticon-chart', title: 'Printing', link: 'services/printing' },
+    // { icon: 'flaticon-graph-1', title: 'Marketing Strategy' },
+    // { icon: 'flaticon-money', title: 'Financial Plan' },
+    { icon: 'flaticon-diamond', title: 'Raw material', link: 'services/material' },
+    // { icon: 'flaticon-teamwork-1', title: 'NGO', ink: '/ngo' },
+    { icon: 'flaticon-diagram', title: 'Renewable Energy', link: 'services/energy' },
+    { icon: 'flaticon-setup', title: 'Building', link: 'services/building'  },
+    { icon: 'flaticon-pie-chart', title: 'Website Consulting', link: 'services/Websites'},
 ]
 
-export default () => {
 
+export default () => {
     return (
-        <section className="service-section section-gap">
+        <section className="service-section shape-style-two service-line-shape section-gap grey-bg">
             <div className="container">
                 {/* Section Title */}
                 <div className="section-title text-center both-border mb-50">
-                    <span className="title-tag">Most Features</span>
+                    <span className="title-tag">Comany Services</span>
                     <h2 className="title">We Provide Most Exclusive <br /> Service For Business</h2>
                 </div>
                 {/* Services Boxes */}
                 <div className="row service-boxes justify-content-center">
                     {serviceblock.map((item, i) => (
-                        <div key={i} className="col-lg-4 col-sm-6 col-10 wow fadeInRight" data-wow-duration="1500ms" data-wow-delay="700ms">
-                            <div className="service-box-three border-0">
+                        <div key={i} className="col-lg-4 col-sm-6 col-10 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="1000ms">
+                            <div className="service-box-two text-center">
                                 <div className="icon">
-                                    <img src={item.icon} alt="" />
+                                    <i className={item.icon} />
                                 </div>
-                                <h3><Link to="/servicedetail">{item.title}</Link></h3>
-                                <p>{item.text}</p>
-                                <Link to="/servicedetail" className="service-link">
-                                    <i className="fal fa-long-arrow-right" />
+                                <h3><Link to={"/" + item.link}> {item.title}</Link></h3>
+                                <Link to={"/" + item.link} className="service-link">
+                                    <i className="fal fa-arrow-right" />
                                 </Link>
                             </div>
                         </div>
                     ))}
                 </div>
+            </div>
+            <div className="line-one">
+                <img src={line1} alt="line-shape" />
+            </div>
+            <div className="line-two">
+                <img src={line2} alt="line-shape" />
             </div>
         </section>
     )
